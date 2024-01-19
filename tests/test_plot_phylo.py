e3aad24ccb8ecabdd63b9a8fed3eb5de5a84f096
Katy Brown <kab84@cam.ac.uk>
2024-01-19 19:41:14 +0000

    tests for rev_align and draw_tree

--- tests/test_plot_phylo.py
+++ tests/test_plot_phylo.py
@@ -4,11 +4,21 @@ import matplotlib.pyplot as plt
 import plot_phylo
 import pytest
 from test_plot_phylo_data import (test_plot_phylo_vars,
-                                 test_plot_phylo_list,
-                                 test_plot_phylo_nams)
+                                  test_plot_phylo_list,
+                                  test_plot_phylo_nams,
+                                  test_draw_tree_vars,
+                                  test_draw_tree_list,
+                                  test_draw_tree_nams,
+                                  test_rev_align_vars,
+                                  test_rev_align_list,
+                                  test_rev_align_nams)
 from test_get_boxes_data import (test_get_boxes_axes,
                                  test_get_boxes_texts,
                                  test_get_boxes_results)
+import ete3
+import pickle
+import os
+import shutil
 
 
 # Tests all parameters with plot_phylo with three different trees
@@ -61,6 +71,7 @@ def test_plot_phylo_params(xpos,
                           font_size=font_size,
                           line_col=line_col,
                           line_width=line_width)
+    os.mkdir("test_temp")
     plt.savefig("test_temp/%s_%s.png" % (ID, tree_stem), bbox_inches='tight')
     plt.close()
     
@@ -68,9 +79,128 @@ def test_plot_phylo_params(xpos,
     # Compare the Matplotlib figures as images
     result = compare_images("test_temp/%s_%s.png" % (ID, tree_stem),
                             exp, tol=10)
-
+    shutil.rmtree("test_temp")
     # Assert that the images are similar
     assert result is None
+
+
+@pytest.mark.parametrize(test_draw_tree_vars,
+                         test_draw_tree_list,
+                         ids=test_draw_tree_nams)
+@pytest.mark.parametrize("tree, ylim", [["examples/primates.nw", 11],
+                                        ["examples/basic_tree.nw", 5],
+                                        ['examples/big_tree.nw', 300]])
+def test_draw_tree_params(x,
+                          y,
+                          x0,
+                          ps,
+                          width,
+                          depth,
+                          align_tips,
+                          rev_align_tips,
+                          branch_lengths,
+                          reverse,
+                          appearance,
+                          expected,
+                          ID,
+                          tree,
+                          ylim):
+    
+    T = ete3.Tree(tree)
+    tree_stem = tree.split("/")[-1].split(".")[0]
+
+    f = plt.figure(figsize=(10, 20))
+    a = f.add_subplot(111)
+    a.set_xlim(-10, 20)
+    a.set_ylim(-1, ylim)
+    for nam in T.get_leaf_names():
+        if nam not in appearance['label_dict']:
+            appearance['label_dict'][nam] = nam
+        if nam not in appearance['col_dict']:
+            appearance['col_dict'][nam] = 'black'
+
+    test_obj = plot_phylo.draw_tree(tree=T, ax=a,
+                                    x=x,
+                                    y=y,
+                                    x0=x0,
+                                    ps=ps,
+                                    height=ylim-1,
+                                    width=width,
+                                    depth=depth,
+                                    align_tips=align_tips,
+                                    rev_align_tips=rev_align_tips,
+                                    branch_lengths=branch_lengths,
+                                    reverse=reverse,
+                                    appearance=appearance)
+    ytest = test_obj[0]
+    y2test = test_obj[1]
+    ps = test_obj[2]
+    v0 = [v[0] for v in ps]
+    v1 = [str(v[1]) for v in ps]
+    plt.close()
+
+    test_dat = [ytest, y2test, v0, v1]
+    e0 = expected.replace(".pickle", "_%s.pickle" % tree_stem)
+    expected_obj = pickle.load(open(e0, "rb"))
+    assert expected_obj == test_dat
+
+
+@pytest.mark.parametrize(test_rev_align_vars,
+                         test_rev_align_list,
+                         ids=test_rev_align_nams)
+@pytest.mark.parametrize("tree, ylim", [["examples/primates.nw", 11],
+                                        ["examples/basic_tree.nw", 5],
+                                        ['examples/big_tree.nw', 300]])
+def test_reverse_align_params(x,
+                              y,
+                              x0,
+                              width,
+                              depth,
+                              branch_lengths,
+                              reverse,
+                              appearance,
+                              expected,
+                              ID,
+                              tree,
+                              ylim):
+    
+    T = ete3.Tree(tree)
+    tree_stem = tree.split("/")[-1].split(".")[0]
+
+    f = plt.figure(figsize=(10, 20))
+    a = f.add_subplot(111)
+    a.set_xlim(-10, 20)
+    a.set_ylim(-1, ylim)
+    for nam in T.get_leaf_names():
+        if nam not in appearance['label_dict']:
+            appearance['label_dict'][nam] = nam
+        if nam not in appearance['col_dict']:
+            appearance['col_dict'][nam] = 'black'
+
+    _, _, ps = plot_phylo.draw_tree(tree=T, ax=a,
+                                    x=x,
+                                    y=y,
+                                    x0=x0,
+                                    ps=[],
+                                    height=ylim-1,
+                                    width=width,
+                                    depth=depth,
+                                    align_tips=True,
+                                    rev_align_tips=True,
+                                    branch_lengths=branch_lengths,
+                                    reverse=reverse,
+                                    appearance=appearance)
+    plt.close()
+    reverse = plot_phylo.reverse_align(a, ps, True)
+    
+    v0 = [v[0] for v in reverse]
+    v1 = [str(v[1]) for v in reverse]
+    v2 = [[str(w.get_xdata()) + str(w.get_ydata())
+           for w in v[2]] for v in reverse]
+    e0 = expected.replace(".pickle", "_%s.pickle" % tree_stem)
+    test_dat = [v0, v1, v2]
+    expected_obj = pickle.load(open(e0, "rb"))
+    assert expected_obj == test_dat
 
 
 @pytest.mark.parametrize("ax, texts, expected_result",
@@ -81,12 +211,6 @@ def test_get_boxes(ax, texts, expected_result):
     boxes = plot_phylo.get_boxes(ax, texts)
     assert boxes == expected_result
 
-
-def test_draw_tree():
-    pass
-
-def test_reverse_align():
-    pass
 
 def test_draw_scale_bar():
     pass
--- tests/test_plot_phylo_data.py
+++ tests/test_plot_phylo_data.py
@@ -9,35 +9,62 @@ ac = plot_phylo.plot_phylo.__code__.co_argcount
 # Argument names, excluding variables defined within the function and
 # arguments with no default values
 varis = list(plot_phylo.plot_phylo.__code__.co_varnames[
-             ac-len(defaults):ac-len(defaults)+len(defaults)])
+             ac-len(defaults):ac])
 
 variD = dict(zip(varis, defaults))
 
 varis.remove('height')
 variD.pop('height')
 
+defaults_draw_tree = plot_phylo.draw_tree.__defaults__
+ac_draw_tree = plot_phylo.draw_tree.__code__.co_argcount
+varis_draw_tree = list(plot_phylo.draw_tree.__code__.co_varnames[
+    ac_draw_tree-len(defaults_draw_tree):ac_draw_tree])
+varis_draw_tree.remove('height')
 
-tests = [{},
-         {'xpos': 1, 'ypos': 1},
-         {'show_axis': False},
-         {'show_support': False},
-         {'align_tips': True},
-         {'rev_align_tips': True},
-         {'branch_lengths': False},
-         {'scale_bar': False},
-         {'scale_bar_width': 6},
-         {'reverse': True},
-         {'rev_align_tips': True, 'reverse': True},
-         {'col_dict': {'Homo sapiens': 'blue'}},
-         {'label_dict': {'Homo sapiens': 'human'}},
-         {'font_size': 20},
-         {'line_col': 'orange'},
-         {'line_width': 5}]
+varis_rev_align = copy.deepcopy(varis_draw_tree)
+varis_rev_align.remove('align_tips')
+varis_rev_align.remove('rev_align_tips')
+varis_rev_align.remove('ps')
 
+tests_plot_phylo = [{},
+                    {'xpos': 1, 'ypos': 1},
+                    {'show_axis': False},
+                    {'show_support': False},
+                    {'align_tips': True},
+                    {'rev_align_tips': True},
+                    {'branch_lengths': False},
+                    {'scale_bar': False},
+                    {'scale_bar_width': 6},
+                    {'reverse': True},
+                    {'rev_align_tips': True, 'reverse': True},
+                    {'col_dict': {'Homo sapiens': 'blue'}},
+                    {'label_dict': {'Homo sapiens': 'human'}},
+                    {'font_size': 20},
+                    {'line_col': 'orange'},
+                    {'line_width': 5}]
+
+tests_draw_tree = [{},
+                  {'xpos': 1, 'ypos': 1},
+                  {'show_axis': False},
+                  {'align_tips': True},
+                  {'rev_align_tips': True},
+                  {'branch_lengths': False},
+                  {'scale_bar': False},
+                  {'scale_bar_width': 6},
+                  {'reverse': True},
+                  {'rev_align_tips': True, 'reverse': True},
+                  {'appearance': {'col_dict': {'Homo sapiens': 'blue'},
+                                  'label_dict': {'Homo sapiens': 'human'},
+                                  'font_size': 20,
+                                  'line_col': 'orange',
+                                  'line_width': 5,
+                                  'show_support': True}},
+                  {'depth': [2, 2, 2]}]
 
 test_plot_phylo_list = []
 test_plot_phylo_nams = []
-for test in tests:
+for test in tests_plot_phylo:
     testnam = "_".join(test.keys())
     if len(testnam) == 0:
         testnam = "basic"
@@ -50,3 +77,49 @@ for test in tests:
     test_plot_phylo_nams.append(testnam)
 test_plot_phylo_vars = ",".join(varis + ['expected_figure', 'ID'])
 
+test_draw_tree_list = []
+test_draw_tree_nams = []
+for test in tests_draw_tree:
+    testnam = "draw_tree_%s" % "_".join(test.keys())
+    curr_dict = copy.deepcopy(variD)
+    curr_dict['x'] = curr_dict['xpos']
+    curr_dict['x0'] = curr_dict['xpos']
+    curr_dict['y'] = curr_dict['ypos']
+    curr_dict['ps'] = []
+    curr_dict['appearance'] = dict()
+    for var in ['col_dict', 'label_dict', 'font_size',
+                'line_col', 'line_width', 'show_support']:
+        curr_dict['appearance'][var] = curr_dict[var]
+    curr_dict['depth'] = [5, 5, 5]
+    curr_dict.update(test)
+    pass_vals = [curr_dict[v] for v in varis_draw_tree]
+    pass_vals.append('tests/test_objects/%s.pickle' % testnam)
+    pass_vals.append(testnam)
+    test_draw_tree_list.append(pass_vals)
+    test_draw_tree_nams.append(testnam)
+test_draw_tree_vars = ",".join(varis_draw_tree + ['expected', 'ID'])
+
+
+test_rev_align_list = []
+test_rev_align_nams = []
+for test in tests_draw_tree:
+    testnam = "rev_align_%s" % "_".join(test.keys())
+    if 'align_tips' not in testnam:
+        curr_dict = copy.deepcopy(variD)
+        curr_dict['x'] = curr_dict['xpos']
+        curr_dict['x0'] = curr_dict['xpos']
+        curr_dict['y'] = curr_dict['ypos']
+        curr_dict['appearance'] = dict()
+        for var in ['col_dict', 'label_dict', 'font_size',
+                    'line_col', 'line_width', 'show_support']:
+            curr_dict['appearance'][var] = curr_dict[var]
+        curr_dict.update(test)
+        curr_dict['depth'] = [5, 5, 5]
+        curr_dict.pop('rev_align_tips')
+        curr_dict.pop('align_tips')
+        pass_vals = [curr_dict[v] for v in varis_rev_align]
+        pass_vals.append('tests/test_objects/%s.pickle' % testnam)
+        pass_vals.append(testnam)
+        test_rev_align_list.append(pass_vals)
+        test_rev_align_nams.append(testnam)
+test_rev_align_vars = ",".join(varis_rev_align + ['expected', 'ID'])
