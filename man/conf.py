c4a4f81b8f05adc5647fd38b1deb35b526cc9d29
Katy Brown <kab84@cam.ac.uk>
2024-10-01 16:16:39 +0100

    achanges to pass flake8 linting

--- man/conf.py
+++ man/conf.py
@@ -1,18 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
 import sys
 import os
+import sphinx_rtd_theme
 sys.path.insert(0, "../plot_phylo")
 sys.path.insert(0, os.path.abspath(".."))
 autodoc_mock_imports = ['ete3']
@@ -50,7 +38,6 @@ root_doc = 'contents'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
--- plot_phylo/__init__.py
+++ plot_phylo/__init__.py
@@ -1,3 +1,1 @@
-from plot_phylo.plot_phylo import *
-
 __author__ = 'Katy Brown, Duncan Cross'
